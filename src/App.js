89264762f2c5520b85c80129e6b9686d618e6c2e
DESKTOP-6I7R7M2\INUC_DESIGN_01 <thanatos6737@naver.com>
2024-01-24 11:29:26 +0900

    #2 할일 리스트 추가 기능 작업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [toDo, setToDo] = useState();
+  const [list, setList] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setList((currentToDos) => [toDo, ...currentToDos]);
+  };
+  console.log('list', list);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="할 일 입력" />
+        <button>입력 완료</button>
+      </form>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
