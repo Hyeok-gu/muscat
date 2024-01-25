0ea5a72fd2472b2c874f3750a7860a8f20cf6a25
DESKTOP-6I7R7M2\INUC_DESIGN_01 <thanatos6737@naver.com>
2024-01-25 14:53:13 +0900

    #6 return 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 
 function App() {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
-
 export default App;
